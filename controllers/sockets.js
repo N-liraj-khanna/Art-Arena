27b024df609fb3ea09d9520b854ffb9a1ce9a553
N Liraj Khanna <lirajkhanna2002@gmail.com>
2023-01-10 01:02:10 +0530

    fixes of socket connection

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -1,3 +1,14 @@
-module.exports.init=(server)=>{
+const socketio = require("socket.io");
 
-}
+const Room = require("./Rooms");
+const Canvas = require("./Canvas");
+const Disconnect = require("./Disconnect");
+const Game = require("./Game");
+
+module.exports.init = (server) => {
+  const io = socketio(server);
+  io.on("connection", function (socket) {
+    console.log("User Connected");
+  });
+  io.emit("connection")
+};
