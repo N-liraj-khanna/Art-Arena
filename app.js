5ab89507847bc0def05930f4b197b4a62e327db2
N Liraj Khanna <lirajkhanna2002@gmail.com>
2023-01-10 00:22:02 +0530

    Sockets, Event Emitter & EJS Connected

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-// Imports
+// Imports Packages
 const express = require("express");
+const { EventEmitter } = require("events");
 require("dotenv").config({ path: "config.env" });
+
+// Import Files
+const sockets = require("./controllers/sockets");
 
 // Constants
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+global.round = new EventEmitter();
+global.games = {};
+global.BONUS = 250;
+global.MAX_POINTS = 500;
+
 // Middlewares
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// Routes
 app.get("/", (req, res) => {
-  return res.send("Welcome to Art Arena");
+  const roomID = req.query.id;
+  return res.render('index', { roomID });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server up and running on port ${PORT}`);
+// Configurations
+const server = app.listen(PORT, () => {
+  console.log(`Server up and running on port ${server.address().port}`);
 });
+
+sockets.init(server);
